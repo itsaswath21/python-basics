741ecac66327d7698cbc69550f27856e592a6f4c
Aswath Sreedhar <itsaswath123@gmail.com>
2026-06-28 18:36:54 +0400

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial not defined for negative numbers"
+    if n == 0 or n == 1:
+        return 1
+    
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    
+    return result
+
+
+num = int(input("Enter a number: "))
+print("Factorial =", factorial(num))
